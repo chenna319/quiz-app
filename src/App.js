6288c8e0ba9710ba48dd8f4ac791546816b414e3
chennakesava <110022346+chenna319@users.noreply.github.com>
2024-10-09 07:33:34 +0530

    route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import "./App.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import QuizGame from "./components/quiz-game";
 import Home from "./components/home";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/quiz-game",
-      element: <QuizGame />,
-    },
-  ]);
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/quiz-game" element={<QuizGame />} />
+      </Routes>
+    </Router>
   );
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element: <Home />,
+  //   },
+  //   {
+  //     path: "/quiz-game",
+  //     element: <QuizGame />,
+  //   },
+  // ]);
+  // return (
+  //   <>
+  //     <RouterProvider router={router} />
+  //   </>
+  // );
 }
 
 export default App;
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,14 +1,16 @@
 import { QRCodeCanvas } from "qrcode.react";
 import React from "react";
+import "../../App.css";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
+    <div className="qr-container">
       <h1>QuizGame</h1>
-      <QRCodeCanvas
-        value="https://dour-blade-da4.notion.site/Indroyd-Assignment-Reference-Document-116573730b5a80c8a925cd4ee4515372"
-        size={250}
-      />
+      <QRCodeCanvas value="https://localhost:3000/quiz-game" size={250} />
+      <Link to="/quiz-game">
+        <button type="button">Game</button>
+      </Link>
     </div>
   );
 };
